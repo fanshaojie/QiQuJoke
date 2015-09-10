1d3d86a0d2d6efd7edcc0f5cac837b9b8948870b
chenhui <chenhui1721@163.com>
2015-09-10 16:25:01 +0800

    测试

---
+++ QiQuJoke/QiQuJoke/STScratchView.h
@@ -0,0 +1,19 @@
+//
+//  STScratchView.h
+//  STScratchView
+//
+//  Created by Sebastien Thiebaud on 12/17/12.
+//  Copyright (c) 2012 Sebastien Thiebaud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STScratchView : UIView
+
+@property (nonatomic, assign) float sizeBrush;
+@property (nonatomic, strong) UIView *hideView;
+
+- (void)setHideView:(UIView *)hideView;
+- (void)setAutomaticScratchCurve:(UIBezierPath *)curvePath duration:(float)duration;
+
+@end
